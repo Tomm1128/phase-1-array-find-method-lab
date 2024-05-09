d7369bb34e5969ff82dc286e0eaefa691d6cb8d3
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-09 09:16:50 -0400

    Create superBowlWin function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+const superbowlWin = array => {
+  const yearWon = array.find(findWin)
+  if (typeof yearWon === 'undefined'){
+    return yearWon
+  } else {
+    return yearWon.year
+  }
+}
+
+
+const findWin = element => element.result === "W"
